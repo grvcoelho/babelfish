d0c939dd224f8f44b57418b2db6b7862121498d0
Guilherme Rv Coelho <guilhermervcoelho@gmail.com>
2016-01-22 20:42:47 -0200

    Add dictionaries fixture

---
+++ test/fixtures/dictionaries.js
@@ -0,0 +1,24 @@
+const en = {
+	'SERVER_ERROR': 'There has been an error',
+	'NOTIFICATIONS': [
+		'No notifications',
+		'1 notification',
+		'%s notifications'
+	]
+};
+
+const pt = {
+	'SERVER_ERROR': 'Houve um erro',
+	'NOTIFICATIONS': [
+		'Nenhuma notifação',
+		'1 notificação',
+		'%s notificações'
+	]
+};
+
+const dictionaries = {
+	'pt-BR': pt,
+	'en-US': en
+};
+
+export default dictionaries;
