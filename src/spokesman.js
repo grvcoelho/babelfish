bf2f0198f83188e1b42ec22fc647a0b2f19d19ee
Guilherme Rv Coelho <guilhermervcoelho@gmail.com>
2016-01-22 22:33:05 -0200

    Add addDictionary function
    grvcoelho/spokesman#4

--- src/spokesman.js
+++ src/spokesman.js
@@ -1,6 +1,24 @@
+import is from 'check-types';
+
 class Spokesman {
 	constructor(dictionaries = {}) {
 		this.dictionaries = dictionaries;
+	}
+
+	addDictionary(name, dictionary) {
+		if (is.undefined(name) || is.not.string(name) || is.emptyString(name)) {
+			throw new Error('The dictionary name must exist');
+		}
+
+		if (is.undefined(dictionary) || is.not.object(dictionary)) {
+			throw new Error('The dictionary must be an object');
+		}
+
+		if (is.not.undefined(this.dictionaries[name])) {
+			throw new Error('The dictionary already exists');
+		}
+
+		this.dictionaries[name] = dictionary;
 	}
 }
 
