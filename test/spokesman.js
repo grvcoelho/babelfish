d3b100db6762bd7adae5025134e4247ef2009037
Guilherme Rv Coelho <guilhermervcoelho@gmail.com>
2016-01-22 22:33:05 -0200

    Add addDictionary function
    grvcoelho/spokesman#4

--- test/spokesman.js
+++ test/spokesman.js
@@ -5,9 +5,38 @@ import is from 'check-types';
 import Spokesman from '../src/spokesman';
 import dictionaries from './fixtures/dictionaries';
 
-
 test('constructor', t => {
 	const spokesman = new Spokesman(dictionaries);
 
 	t.ok(is.object(spokesman.dictionaries));
+});
+
+test('add a dictionary', t => {
+	const spokesman = new Spokesman(dictionaries);
+	const fr = { 'SUGAR': 'Sucre' };
+
+	spokesman.addDictionary('fr-CA', fr);
+
+	t.is(spokesman.dictionaries['fr-CA'], fr);
+});
+
+test('add a dictionary with an existent name', t => {
+	const spokesman = new Spokesman(dictionaries);
+	const fr = { 'SUGAR': 'Sucre' };
+
+	t.throws(() => spokesman.addDictionary('en-US', fr));
+});
+
+test('add a dictionary without a valid name', t => {
+	const spokesman = new Spokesman(dictionaries);
+	const fr = { 'SUGAR': 'Sucre' };
+
+	t.throws(() => spokesman.addDictionary('', fr));
+});
+
+test('add a dictionary without a valid dictionary', t => {
+	const spokesman = new Spokesman(dictionaries);
+	const fr = [1, 2];
+
+	t.throws(() => spokesman.addDictionary('fr-CA', fr));
 });
