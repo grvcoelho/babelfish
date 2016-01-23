2573c4aa967d8ec16eccea7ea9bf834e79dc93bc
Guilherme Rv Coelho <guilhermervcoelho@gmail.com>
2016-01-23 09:05:53 -0200

    Add translate and translateWithPluralisation methods
    grvcoelho/spokesman#10

--- test/spokesman.js
+++ test/spokesman.js
@@ -9,8 +9,8 @@ let spokesman;
 
 test.beforeEach(() => {
 	spokesman = new Spokesman({
-		'pt': dictionaries.pt,
-		'en': dictionaries.en
+		pt: dictionaries.pt,
+		en: dictionaries.en
 	});
 });
 
@@ -50,3 +50,25 @@ test('use a dictionary', t => {
 test('use a dictionary that does not exist', t => {
 	t.throws(() => spokesman.useDictionary('es'));
 });
+
+test('translate a simple term', t => {
+	spokesman.useDictionary('en')
+
+	t.is(spokesman.translate('FRUIT_LIST'), 'List of fruits');
+});
+
+test('translate a simple term that does not exist', t => {
+	spokesman.useDictionary('en');
+
+	t.is(spokesman.translate('SALAD'), 'SALAD');
+});
+
+test('translate a term with pluralisation', t => {
+	spokesman.useDictionary('en');
+
+	t.is(spokesman.translate('APPLE'), '1 apple');
+	t.is(spokesman.translate('APPLE', 0), 'No apples');
+	t.is(spokesman.translate('APPLE', 1), '1 apple');
+	t.is(spokesman.translate('APPLE', 2), '2 apples');
+	t.is(spokesman.translate('APPLE', 17), '17 apples');
+})
