1ad0f95cd40b68470909bfe40fcecc335bff2761
Guilherme Rv Coelho <guilhermervcoelho@gmail.com>
2016-02-16 14:38:53 -0300

    Add tests

---
+++ test/fixtures/dictionaries.js
@@ -0,0 +1,43 @@
+const en = {
+	'FRUIT_LIST': 'List of fruits',
+	'APPLE': [
+		'No apples',
+		'1 apple',
+		'%s apples'
+	],
+	'RANDOM_FRUIT': function getRandomFruit() {
+		return 'banana';
+	}
+};
+
+const pt = {
+	'FRUIT_LIST': 'Lista de frutas',
+	'APPLE': [
+		'Nenhuma maçã',
+		'1 maçã',
+		'%s maçãs'
+	],
+	'RANDOM_FRUIT': function getRandomFruit() {
+		return 'banana';
+	}
+};
+
+const fr = {
+	'FRUIT_LIST': 'Liste de fruits',
+	'APPLE': [
+		'Sans pommes',
+		'1 pomme',
+		'%s pommes'
+	],
+	'RANDOM_FRUIT': function getRandomFruit() {
+		return 'banane';
+	}
+};
+
+const dictionaries = {
+	pt,
+	en,
+	fr
+};
+
+export default dictionaries;
